6c74fc4b847697a065861dbf09e34bdd56465582
tuyizereherve9-web <tuyizereherve9@gmail.com>
2025-09-25 22:33:04 +0200

    Delete queue2.py

--- queue2.py
+++
@@ -1,6 +0,0 @@
-from collections import deque
-queue = deque(["Bus1","Bus2","Bus3","Bus4","Bus5","Bus6","Bus7","Bus8"])
-print("Initial queue:", list(queue))
-departed = queue.popleft()
-print("Departed bus:", departed)
-print("Queue now:", list(queue))
