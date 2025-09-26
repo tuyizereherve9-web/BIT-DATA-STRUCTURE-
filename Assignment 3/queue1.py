f89d31089549397e20e93b28d0ce92677c477a3f
tuyizereherve9-web <tuyizereherve9@gmail.com>
2025-09-26 13:26:00 +0200

    Delete Assignment 3/queue1.py

--- Assignment 3/queue1.py
+++
@@ -1,8 +0,0 @@
-from collections import deque
-queue = deque([1,2,3,4,5,6,7,8,9])
-print("Initial queue:", list(queue))
-for _ in range(6):
-    queue.popleft()
-
-print("Queue after serving 6 citizens:", list(queue))
-print("Front of the queue:", queue[0])
